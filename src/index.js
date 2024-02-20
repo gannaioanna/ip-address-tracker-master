777a3e49ce291279e1f9a6945dfbdaa057729271
Anna Ignateva <gannaioannovna25@gmail.com>
2024-02-20 23:59:39 +0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('hello from js');
+import { validatIp } from './helpers';
+
+const ipInput = document.querySelector('.search-bar__input');
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', getData);
+ipInput.addEventListener('keydown', handleKey);
+
+function getData() {
+  if (validatIp(ipInput.value)) {
+    fetch(
+      `https://geo.ipify.org/api/v2/country?apiKey=at_pb8NXycKLPcjlRdQHHnN5Kk0vbsdv&ipAddress=${ipInput.value}`
+    )
+      .then((response) => response.json())
+      .then(console.log);
+  }
+}
+
+function handleKey(e) {
+  if (e.key === 'Enter') {
+    getData();
+  }
+}
