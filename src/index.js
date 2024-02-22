513ae32a37edc99f9fe1a6b9ddd262c14ca0c3a2
Anna Ignateva <gannaioannovna25@gmail.com>
2024-02-22 20:19:25 +0400

    fouth commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import 'babel-polyfill';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import { addTileLayer, getAddress, validateIp } from './helpers';
+import { addOffset, addTileLayer, getAddress, validateIp } from './helpers';
 import icon from '../images/icon-location.svg';
 
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
+
 const ipInfo = document.querySelector('#ip');
 const locationInfo = document.querySelector('#location');
 const timezoneInfo = document.querySelector('#timezone');
@@ -13,16 +14,17 @@ const ispInfo = document.querySelector('#isp');
 
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
+
 const markerIcon = L.icon({
   iconUrl: icon,
   iconSize: [30, 40],
-  iconAnchor: [22, 94],
 });
 
 const mapArea = document.querySelector('.map');
 const map = L.map(mapArea, {
   center: [51.505, -0.09],
   zoom: 13,
+  zoomControl: false,
 });
 addTileLayer(map);
 
@@ -49,4 +51,10 @@ function setInfo(mapData) {
 
   map.setView([lat, lng]);
   L.marker([lat, lng], { icon: markerIcon }).addTo(map);
+  if (matchMedia('(max-width: 1023px)').matches) {
+    addOffset(map);
+  }
 }
+document.addEventListener('DOMContentLoaded', () => {
+  getAddress('102.22.22.1').then(setInfo);
+});
